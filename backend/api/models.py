255a97d2f4c3e8dd9acba84f0a76b6af555b9afb
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-01-31 11:53:33 +0300

    Added an Audit_trail model and serializer  to keep track of the actions on an issue

--- backend/api/models.py
+++ backend/api/models.py
@@ -49,7 +49,35 @@ class Issue(models.Model):
     lecturer = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,null = True, related_name = "lecturer",limit_choices_to={'role':'lecturer'})
     registrar = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,null = True, related_name = "registrar",limit_choices_to={'role':'academic_registrar'})
     
+    class Meta:
+        ordering = ['-updated_at','-created_at']
+    
     def __str__(self):
         return self.issue_type
 
 
+
+class Audit_Trail(models.Model):
+    ACTION_CHOICES = [
+        ('created','Created'),
+        ('assigned','Assigned'),
+        ('resolved','Resolved'),
+        ('updated','Updated'),
+        ('forwarded','Forwarded'),
+        ('closed','Closed'),
+    ]
+    
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE,related_name='audit_trail',)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    action = models.CharField(max_length=20, choices = ACTION_CHOICES,default = 'created')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    comment = models.TextField(blank=True,null = True)
+    previous_status = models.CharField(blank = True, null = True,max_length=30)
+    new_status = models.CharField(blank = True, null = True,max_length=30)
+    
+    def __str__(self):
+        return f"{self.user.username}-{self.action} on {self.issue} at {self.updated_at}"
+    
+    class Meta:
+        ordering = ['-updated_at','-created_at']
