7dffdd64befdabdff91541819d6791711a08eb62
Larrie-Tau <kasibantelarry@gmail.com>
2025-03-15 13:39:22 +0300

    added an image in the asset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import IssueList from "./components/IssueList"; // Previously created component
-import DepartmentList from "./components/DepartmentList"; // New component
 
 function App() {
   return (
     <div className="App">
-      <h1>My Django+React App</h1>
-      {/* Display Departments */}
-      <DepartmentList />
-      {/* Optionally display Issues */}
-      <IssueList />
     </div>
   );
 }
